8e2ffe24270b36b5133a062842db55621688c494
Soozini <kkssjj9607@gmail.com>
2019-05-17 15:48:33 +0900

    별점 추가

--- client/src/pages/Buyer/create-review/create-review.ts
+++ client/src/pages/Buyer/create-review/create-review.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,Input, EventEmitter, Output } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 // import { Events } from 'ionic-angular';
 
@@ -22,6 +22,8 @@ import {UserStore} from "../../../core/model/UserStore";
 
 })
 export class CreateReviewPage {
+  @Input() rating : number;
+  @Output() ratingChange : EventEmitter<number> = new EventEmitter();
   userStore: UserStore;
 
   // rating : number= 4;
@@ -36,15 +38,53 @@ export class CreateReviewPage {
 
   }
 
+  rate(index : number){
+    this.rating= index;
+    this.ratingChange.emit(this.rating);
+
+  }
+
+  getColor(index : number){
+    if(this.isAboveRating(index)){
+      return COLORS.GREY;
+    }
+    switch(this.rating) {
+      case 1:
+        case 2:
+          return COLORS.RED;
+      case 3:
+        return COLORS.YELLOW;
+      case 4:
+      case 5:
+        return COLORS.GREEN;
+      default:
+        return COLORS.GREY;
+
+    }
+  }
+  isAboveRating(index: number) : boolean{
+    return index> this.rating;
+  }
+
   ionViewDidLoad() {
     console.log('ionViewDidLoad CreateReviewPage');
   }
   back() {
-    this.navCtrl.pop();
+    this.navCtrl.setRoot('StoreDetailComponent');
   }
 
   add(){
 
     this.navCtrl.setRoot("StoreDetailComponent");
   }
+
+
+}
+
+enum COLORS{
+
+  GREY= "#E0E0E0",
+  GREEN="#76FF03",
+  YELLOW="#FFCA28",
+  RED="#DD2C00"
 }
