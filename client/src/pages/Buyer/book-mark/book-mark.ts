b78f24406c89eb4829fbec65d237293d1b84096a
Soozini <kkssjj9607@gmail.com>
2019-06-04 14:33:47 +0900

    구매자 즐겨찾기 페이지, 상점리스트 페이지 수정

--- client/src/pages/Buyer/book-mark/book-mark.ts
+++ client/src/pages/Buyer/book-mark/book-mark.ts
@@ -183,9 +183,14 @@ export class BookMarkPage {
     ) 
   }
 
+  goToPage(pageName: string, sellerId?: string) {
+    switch(pageName){
+      case 'store-detail':
+        this.navCtrl.push('BuyerStoreDetailPage', {sellerId: sellerId});
+    }
+  }
 
-
-} 
+}
 
 
 class bookmarkStore {
